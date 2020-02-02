6ab359fc51633eb0c9756c54285c1f13b3b45b04
Jack <53200690+0II@users.noreply.github.com>
2020-02-02 20:09:29 +0000

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://';
+dbPassword = 'mongodb+srv://jpw:Adminadmin1@cluster0-wbugs.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
